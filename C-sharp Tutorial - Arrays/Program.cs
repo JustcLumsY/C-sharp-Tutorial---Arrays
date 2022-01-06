a86b34b941b4ed368c3f90810097bb0ccf1c02f0
John Espen Thoresen <84008816+JustcLumsY@users.noreply.github.com>
2022-01-06 08:13:33 +0100

    Add project files.

---
+++ C-sharp Tutorial - Arrays/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace C_sharp_Tutorial___Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] luckyNumbers = { 4, 8, 15, 23, 42 };
+            Console.WriteLine(luckyNumbers[0]);
+            string[] friends = new string[10]; //Stores 10 values
+            friends[0] = "Jim";
+            friends[1] = "Kelly";
+
+            Console.ReadLine();
+        }
+    }
+}
